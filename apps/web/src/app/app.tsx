f0aeb4dd543b4ea5103c42021b4b68521121b411
Nick Squires <github@nsquires.me>
2026-06-17 21:15:24 -0400

    feat: wire tailwind v4 into web app

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,12 +1,6 @@
-// Uncomment this line to use CSS modules
-// import styles from './app.module.css';
-import NxWelcome from "./nx-welcome";
-
 export function App() {
   return (
-    <div>
-      <NxWelcome title="web" />
-    </div>
+    <h1 className="text-3xl font-bold underline">Tailwind works</h1>
   );
 }
 
