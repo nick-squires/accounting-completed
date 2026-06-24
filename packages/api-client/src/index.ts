5ca2c66aa470c3a56c78773dad5bc3fb63a64234
Nick Squires <github@nsquires.me>
2026-06-24 09:03:21 -0400

    feat(web): live activity feed on the dashboard

--- packages/api-client/src/index.ts
+++ packages/api-client/src/index.ts
@@ -4,3 +4,4 @@ export { useClients } from "./use-clients";
 export { useIncomeStatement, useIncomeStatementYears } from "./use-income-statement";
 export { useTransactions, useTransactionsYears } from "./use-transactions";
 export { useAccounts } from "./use-accounts";
+export { useActivity } from "./use-activity";
